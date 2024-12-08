c84be4016ba546b670451f3c883256b95cabcb98
ganiyat-bakare <bak24006@byui.edu>
2024-12-08 06:22:47 +0100

    discover places added

---
+++ chamber/data/places.mjs
@@ -0,0 +1,87 @@
+export const places = [  
+    {  
+        "name": "Aso Rock",  
+        "address": "Aso Rock, Abuja",  
+        "cost": "Free",  
+        "description": "A prominent rock formation offering breathtaking views of the city. Ideal for hiking and outdoor activities.",  
+        "photo_url": "aso-rock.webp"  
+    },  
+    {  
+        "name": "Nigerian National Mosque",  
+        "address": "1, Independence Avenue, Abuja",  
+        "cost": "Free (donations appreciated)",  
+        "description": "One of West Africa's largest mosques, notable for its stunning architecture. Guided tours are available.",  
+        "photo_url": "national-mosque.webp"  
+    },  
+    {  
+        "name": "Jabi Lake",  
+        "address": "Jabi, Abuja",  
+        "cost": "Entrance is usually free; boat rentals vary.",  
+        "description": "A serene lake perfect for boating and picnicking, surrounded by beautiful greenery.",  
+        "photo_url": "jabi-lake.webp"  
+    },  
+    {  
+        "name": "Arts and Crafts Village",  
+        "address": "Opposite the Shehu Musa Yar’Adua Centre, Abuja",  
+        "cost": "Free to enter, shopping costs vary.",  
+        "description": "A vibrant marketplace featuring local crafts, art, and souvenirs by local artisans.",  
+        "photo_url": "artscrafts.webp"  
+    },  
+    {  
+        "name": "Nigerian National Museum",  
+        "address": "1, Independence Rd, Garki 2, Abuja",  
+        "cost": "Approximately ₦200 (about \$0.50)",  
+        "description": "Showcases Nigeria's rich cultural heritage with various historical exhibits and artifacts.",  
+        "photo_url": "national-museum.webp"  
+    },  
+    {  
+        "name": "Millennium Park",  
+        "address": "Adjacent to the Nigerian Presidential Complex, Abuja",  
+        "cost": "Free",  
+        "description": "A large park featuring beautiful gardens, walking paths, and picnic areas, great for families and relaxation.",  
+        "photo_url": "millennium-park.webp"  
+    },  
+    {  
+        "name": "Nigerian Cultural Centre",  
+        "address": "Cotonou Crescent, Off Adetokumbo Ademola Crescent, Wuse 2, Abuja",  
+        "cost": "Varies by event (usually free entry for exhibitions)",  
+        "description": "A hub for cultural activities, including performances, workshops, and art exhibitions.",  
+        "photo_url": "cultural-centre.webp"  
+    },  
+    {  
+        "name": "Nike Art Gallery",  
+        "address": "1, Obafemi Awolowo Way, Wuse 2, Abuja",  
+        "cost": "Free entry; costs for art pieces vary.",  
+        "description": "An expansive art gallery showcasing Nigerian arts and crafts, perfect for art lovers and cultural enthusiasts.",  
+        "photo_url": "nike-art-gallery.webp"  
+    },  
+    {  
+        "name": "Maitama District",  
+        "address": "Maitama, Abuja",  
+        "cost": "Free to explore; costs for dining/shopping vary.",  
+        "description": "A luxurious neighborhood known for high-end dining, shopping, and beautiful architecture. Great for leisurely strolls.",  
+        "photo_url": "maitama.webp"  
+    }  ,  
+    {  
+        "name": "Abuja Zoo",  
+        "address": "1, Zoo Road, Asokoro, Abuja",  
+        "cost": "Approximately ₦100 (about \$0.25)",  
+        "description": "A family-friendly zoo featuring a variety of wildlife and educational programs focused on conservation.",  
+        "photo_url": "abuja-zoo.webp"  
+    },  
+    {  
+          
+        "name": "Bungalow Restaurant",  
+        "address": "2, 1st Avenue, Gwarinpa, Abuja",  
+        "cost": "Average meal around ₦2,000 - ₦5,000 (about \$5 - \$12)",  
+        "description": "Enjoy delicious traditional Nigerian dishes in a welcoming atmosphere.",  
+        "photo_url": "bungalow.webp"  
+    },  
+    {  
+        "name": "Nkoyo",  
+        "address": "35, Nile Street, Maitama, Abuja",  
+        "cost": "Average meal around ₦2,000 - ₦5,000 (about \$5 - \$12)",  
+        "description": "Enjoy delicious traditional Nigerian dishes in a welcoming atmosphere.",  
+        "photo_url": "nkoyo.webp"  
+    }   
+]
